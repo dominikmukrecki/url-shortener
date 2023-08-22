cdcb7267e25dbebb3393ce89ef1eb4e6af4a9fe1
D M <dominik@mukrecki.pl>
2023-08-22 23:39:31 +0200

    Update handlers.ts

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,53 +1,41 @@
-import { Request } from 'itty-router';
+import { IRequest as Request } from 'itty-router';
 import { Env } from './config';
-import { fetchOriginalURL, logLinkEntry, logErrorEntry } from './api';
-import { iframeEmbedContent, arrayToQueryString } from './utils';
+import { fetchOriginalURL, logLinkEntry } from './api';
+import { iframeEmbedContent } from './utils';
 
-export async function handleSlugRoute(request: Request, env: Env): Promise<Response> { //look for query in the request
-    const headersArray: { key: string, value: string }[] = [];
-    const additionalHeaders: { [key: string]: string } = {};
-
-    for (const [key, value] of request.headers.entries()) {
-        headersArray.push({ key, value });
-        additionalHeaders[key] = value;
-    }
-
+export async function handleSlugRoute(request: Request, env: Env): Promise<Response> {
     try {
         const { slug } = request.params as { slug: string };
-        const { original_url, id, headers: fetchedHeaders, query: fetchedQuery } = await fetchOriginalURL(slug, env);
+        const { original_url, id, headers: fetchedHeaders, query: fetchedQuery, status } = await fetchOriginalURL(slug, env);
         
-        if (original_url && id) {
-            await logLinkEntry(id, new URL(request.url).searchParams, headersArray, request, env);
-            
-            const queryString = arrayToQueryString(fetchedQuery);
-            const mergedUrl = original_url + (queryString ? '?' + queryString : ''); //you get me wrrong - query from request is a string, but fetched query is an array of objects/ solve this
+        const searchParams = new URL(request.url).searchParams;
+        
+        const fetchedQueryString = new URLSearchParams(fetchedQuery).toString();
+        const existingQueryString = searchParams.toString();
+        const mergedQueryString = existingQueryString ? existingQueryString + '&' + fetchedQueryString : fetchedQueryString;
+        const mergedUrl = original_url + (mergedQueryString ? '?' + mergedQueryString : '');
 
+        if (original_url && id) {
+            await logLinkEntry(id, searchParams, request.headers, request, env, status);
+            
             const responseHeaders = {
-                ...additionalHeaders,
                 ...fetchedHeaders,
-                'Location': mergedUrl // brilliant for now! but existing query from api /:slug is not merged into it
+                'Location': mergedUrl
             };
 
             return new Response('', { status: 302, headers: responseHeaders });
         } else {
-            logErrorEntry(request.url, new URL(request.url).searchParams, headersArray, env);
+            await logLinkEntry(null, searchParams, request.headers, request, env, "ERROR_LINK_NOT_FOUND");
             return iframeEmbedContent(env);
         }
     } catch (error) {
         console.error("Error in handleSlugRoute:", error.message);
-        logErrorEntry(request.url, new URL(request.url).searchParams, headersArray, env);
+        await logLinkEntry(null, new URL(request.url).searchParams, request.headers, request, env, "ERROR_IN_ROUTE_HANDLER");
         return iframeEmbedContent(env);
     }
 }
 
-
-
 export async function handleFallbackRoute(request: Request, env: Env): Promise<Response> {
-    const headersArray: { key: string, value: string }[] = [];
-    for (const [key, value] of request.headers.entries()) {
-        headersArray.push({ key, value });
-    }
-
-    logErrorEntry(request.url, new URL(request.url).searchParams, headersArray, env);
+    await logLinkEntry(null, new URL(request.url).searchParams, request.headers, request, env, "ERROR_IN_FALLBACK");
     return iframeEmbedContent(env);
 }
