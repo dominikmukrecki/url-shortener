18a71289d55c23cf9a80c5511fd1335a5dffe707
D M <dominik@mukrecki.pl>
2023-08-24 07:17:28 +0200

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,15 @@
 import { Env } from './config';
 import { getAuthHeaders } from './auth';
 
+interface DirectusResponse {
+    data: {
+        original_url: string;
+        id: string;
+        request_headers: any;
+        request_query: any;
+    }[];
+}
+
 export async function fetchOriginalURL(slug: string, env: Env): Promise<{ original_url: string | null, id: string | null, request_headers: any, request_query: any }> {
     try {
         const response: Response = await fetch(`${env.DIRECTUS_API_LINKS_ENDPOINT}?filter[slug][_eq]=${slug}&limit=1`, {
@@ -14,7 +23,8 @@ export async function fetchOriginalURL(slug: string, env: Env): Promise<{ origin
                 request_query: {},
             };
         }
-        const data = await response.json();
+        
+        const data = await response.json() as DirectusResponse;
 
         return {
             original_url: data.data && data.data[0] ? data.data[0].original_url : null,
@@ -22,7 +32,7 @@ export async function fetchOriginalURL(slug: string, env: Env): Promise<{ origin
             request_headers: data.data && data.data[0] ? data.data[0].request_headers : {},
             request_query: data.data && data.data[0] ? data.data[0].request_query : {},
         };
-    } catch (error) {
+            } catch (error) {
         console.error(`Error fetching original URL for slug ${slug}:`, error);
         return {
             original_url: null,
